8e1e950051b64bfb722291e9d6aedef000ed4b2e
Recognition2 <gkh998@gmail.com>
2017-01-27 23:40:31 +0100

    Bot is now an echoBot, asynchronously handling all messages. However, it does not work.

--- main.go
+++ main.go
@@ -1,34 +1,139 @@
 // main.go
 // Author: gregory at 24-01-2017
 
-package main 
+package main
 
 import (
-	"gopkg.in/yaml.v2"
-	"io/ioutil"
 	"fmt"
+	"github.com/BurntSushi/toml"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"net/url"
+	"os"
+	"os/signal"
+	"strconv"
+	"sync"
+	"syscall"
+	"time"
 )
 
-type settings struct {
-	Password	string	
-	Admin		string 	
+type Config struct {
+	Apikey     string // Telegram API key
+	Admins     []string
+	UsePolling bool
+	LogLevel   string // how much to log
 }
 
+// Global variables
+// Loggers:
+var logErr = log.New(os.Stderr, "[ERR] ", log.Ldate+log.Ltime+log.Ltime)
+var logWarn = log.New(os.Stdout, "[WARN] ", log.Ldate+log.Ltime)
+var logInfo = log.New(os.Stdout, "[INFO] ", log.Ldate+log.Ltime)
+
 func main() {
-	println("Hello, world!")
-	rawSettings, err := ioutil.ReadFile("settings.conf");
+	os.Exit(mainExitCode())
+}
+
+func mainExitCode() int {
+	// Create logging objects
+
+	// Parse bot configuration
+	var c Config
+	_, err := toml.DecodeFile("settings.toml", &c)
 	if err != nil {
-		println("Error reading configuration file")
+		logErr.Println(err)
 	}
 
-	s := settings{}
-	err = yaml.Unmarshal([]byte(rawSettings), &s)
+	switch c.LogLevel {
+	case "error":
+		logWarn.SetFlags(0)
+		logWarn.SetOutput(ioutil.Discard)
+		fallthrough
+	case "warn":
+		logInfo.SetFlags(0)
+		logInfo.SetOutput(ioutil.Discard)
+		fallthrough
+	case "info":
+	default:
+		logErr.Println("No valid logLevel directive in configuration file")
+		return 1
+	}
+	logInfo.Println("Config file parsed")
+
+	logWarn.Println("This is an example of a warning")
+	bot := Tbot{apikey: c.Apikey}
+	bot.Username, err = bot.getUsername()
 	if err != nil {
-		println("Error parsing configuration")
-		fmt.Printf("%+v\n", err)
+		logErr.Print(err)
 	}
 
-    fmt.Printf("--- settings:\n%v\n\n", s)
-    fmt.Printf("MONKEY BANANAN\n")
+	shouldShutdown := make(chan bool)
 
+	// Start the waitgroup
+	var wg sync.WaitGroup
+
+	// Start all async operations
+	wg.Add(1)
+	go messageMonitor(wg, bot, shouldShutdown)
+
+	// Wait for SIGINT or SIGTERM, then quit
+	done := make(chan bool, 1)
+	sigs := make(chan os.Signal, 2)
+	signal.Notify(sigs, os.Interrupt, syscall.SIGINT)
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		<-sigs // This goroutine will hang here until interrupt is sent
+		logInfo.Println("Shutdown signal received, waiting for goroutines")
+		close(shouldShutdown)
+		// Await all other goroutines, then send Done
+		done <- true
+	}()
+
+	logInfo.Println("All routines have been started, awaiting kill signal")
+
+	// Program will hang here, probably forever
+	<-done
+	// Shutdown initiated, waiting for all goroutines to shut down
+	logInfo.Println("Waiting for async operations to stop...")
+	wg.Wait()
+	logInfo.Println("Everything has shut down, stopping now")
+	return 0
+}
+
+func messageMonitor(wg sync.WaitGroup, bot Tbot, shutdown chan bool) {
+	var offset int64 = 0
+
+outer:
+	for {
+		select {
+		case <-shutdown:
+			break outer
+		default:
+		}
+		updates, err := bot.getUpdates(offset, shutdown)
+		if err != nil {
+			logWarn.Print(err)
+		}
+		for _, k := range updates {
+			fmt.Printf("%#v\n", k)
+			if k.Update_id > offset {
+				offset = k.Update_id
+				go handleMessage(k.Message, bot)
+			}
+		}
+		time.Sleep(time.Second * 10)
+	}
+	logInfo.Println("Stopping message monitor")
+	wg.Done()
+}
+
+func handleMessage(m Message, bot Tbot) {
+	logInfo.Println("Handling message")
+	raw := url.Values{}
+	raw.Set("chat_id", strconv.FormatInt(m.Chat.Id, 10))
+	raw.Add("text", "Hoi. You sent: "+m.Text)
+
+	http.Post(turl+bot.apikey+"/sendMessage", raw.Encode(), nil)
 }
---
+++ tbot.go
@@ -0,0 +1,149 @@
+package main
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"strconv"
+)
+
+type Tbot struct {
+	Username string // Bot username
+	apikey   string // Communication token with Telegram API
+}
+
+type User struct {
+	Id         int    `json:"id"`         // Unique identifier for this user/bot
+	First_name string `json:"first_name"` // User's or bot's first name
+	Last_name  string `json:"last_name"`  // OPTIONAL: User's or bot's last name
+	Username   string `json:"username"`   // OPTIONAL
+}
+
+type Update struct {
+	Update_id int64   `json:"update_id"`         // Unique identifier
+	Message   Message `json:"message,omitempty"` // OPTIONAL:	Message that was sent
+}
+
+type Message struct {
+	Message_id int64  // Unique identifier
+	From       User   // OPTIONAL: May be empty for channels
+	Date       int    // Date message was sent, Unix time
+	Chat       Chat   // Conversation message belongs to
+	Text       string // Raw UTF-8 text of message
+}
+
+type Chat struct {
+	Id       int64  // Unique identifier, max 52 bits.
+	Type     string // One of: "private", "group", “supergroup” or “channel”
+	Title    string // OPTIONAL
+	Username string //OPTIONAL: Not for groups.
+}
+
+type ReceivedUpdates struct {
+	Update []Update
+}
+
+type BasicReturn struct {
+	Ok     bool
+	Result interface{}
+}
+
+var turl = "https://api.telegram.org/bot"
+
+// Get information about self
+func (t Tbot) getUsername() (string, error) {
+	resp, err := http.Get(turl + t.apikey + "/getMe")
+	if err != nil {
+		return "", err
+	}
+
+	self, err := ioutil.ReadAll(resp.Body) // Read stream
+	resp.Body.Close()
+	if err != nil {
+		return "", err
+	}
+
+	// Parse data
+	var thisBot User
+	var result json.RawMessage
+	var isSucces = BasicReturn{
+		Result: &result,
+	}
+
+	err = json.Unmarshal(self, &isSucces)
+	if err != nil {
+		return "", err
+	}
+	if isSucces.Ok != true {
+		return "", errors.New(fmt.Sprintf("Telegram bot token is incorrect: %v", isSucces.Result))
+	}
+
+	err = json.Unmarshal(result, &thisBot)
+	if err != nil {
+		return "", err
+	}
+
+	// Print data
+	logInfo.Printf("Telegram token valid, starting as @%v\n", thisBot.Username)
+	return thisBot.Username, nil
+}
+
+func (t Tbot) getUpdates(offset int64, shutdown chan bool) ([]Update, error) {
+	logInfo.Println("Getting an Update")
+	timeout := 120
+	var arg = "offset=" + strconv.FormatInt(offset, 10) + "&timeout=" + strconv.Itoa(timeout)
+	httpResponse, err := http.Get(turl + t.apikey + "/getUpdates?" + arg)
+	// http.Get has an inherent delay, while doing DNS lookup etc. (guaranteeing that your connection is valid)
+	if err != nil {
+		return nil, err
+	}
+
+	rawData := make(chan []byte)
+	rawDataCompleted := make(chan bool)
+
+	go func() {
+		defer httpResponse.Body.Close()
+		rawdata, err := ioutil.ReadAll(httpResponse.Body) // Read stream
+		if err != nil {
+			logErr.Printf("Error reading from response: %v\n", err)
+			return
+		}
+		rawDataCompleted <- true
+		rawData <- rawdata
+	}()
+
+	select {
+	case <-shutdown:
+		logWarn.Println("Forced to shut down")
+		return nil, nil
+	case <-rawDataCompleted:
+		break
+	}
+
+	raw := <-rawData
+	// Parse raw results
+	var result json.RawMessage
+	var isSucces = BasicReturn{
+		Result: &result,
+	}
+
+	err = json.Unmarshal(raw, &isSucces)
+	if err != nil {
+		return nil, err
+	}
+	if isSucces.Ok != true {
+		logErr.Printf("ERROR from Telegram API: %+v", isSucces.Result)
+		return nil, errors.New("Telegram bot token is incorrect")
+	}
+
+	// Create Update objects
+	updates := make([]Update, 100)
+	err = json.Unmarshal(result, &updates)
+	if err != nil {
+		return nil, err
+	}
+
+	return updates, nil
+}

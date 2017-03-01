50dc51d743b62b965aab6a2df935f15ca668731c
Gregory Kevin Hill <gkh998@gmail.com>
2017-03-01 17:51:21 +0100

    Primary key should envelop multiple columns in personStats chat

--- init.sql
+++ init.sql
@@ -1,26 +1,18 @@
 CREATE TABLE `chats` (
-  `id` bigint unsigned NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  `chatid` bigint unsigned NOT NULL,
-  `name` char(150) COLLATE 'utf8mb4_bin' NOT NULL,
-  `messageTotal` int unsigned NOT NULL,
-  `charTotal` bigint unsigned NOT NULL,
-  `Type` char(30) COLLATE 'utf8mb4_bin' NOT NULL
-) ENGINE='InnoDB' COLLATE 'utf8mb4_bin';
-
-ALTER TABLE `chats`
-ADD UNIQUE `chatid` (`chatid`);
+  `chatid` bigint(20) NOT NULL,
+  `name` char(150) COLLATE utf8mb4_bin NOT NULL,
+  `messageTotal` int(10) unsigned NOT NULL,
+  `charTotal` bigint(20) unsigned NOT NULL,
+  `Type` char(30) COLLATE utf8mb4_bin NOT NULL,
+  PRIMARY KEY (`chatid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
 
 CREATE TABLE `personstats` (
-  `id` bigint NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  `chatfk` bigint(20) unsigned NOT NULL,
-  `personid` bigint NOT NULL,
-  `msgcount` bigint NOT NULL,
-  `charcount` int NOT NULL,
-  FOREIGN KEY (`chatfk`) REFERENCES `chats` (`chatid`) ON DELETE CASCADE
-) ENGINE='InnoDB' COLLATE 'utf8mb4_bin';
-
-ALTER TABLE `personstats`
-ADD UNIQUE `personid` (`personid`);
-
-ALTER TABLE `personstats`
-ADD `name` varchar(64) COLLATE 'utf8mb4_bin' NOT NULL;
+  `chatfk` bigint(20) NOT NULL,
+  `personid` bigint(20) NOT NULL,
+  `msgcount` bigint(20) NOT NULL,
+  `charcount` int(11) NOT NULL,
+  `name` varchar(64) COLLATE utf8mb4_bin NOT NULL,
+  PRIMARY KEY (`chatfk`,`personid`),
+  CONSTRAINT `personstats_ibfk_1` FOREIGN KEY (`chatfk`) REFERENCES `chats` (`chatid`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
